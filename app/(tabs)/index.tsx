893e6f318c8786198c7b5b8bd413bffb602aca83
HumairAli26 <humairali2612@gmail.com>
2026-07-13 15:45:05 +0500

    Added a bottom tab for navigation and basic code for structural and navigational cheking to see if the expo-router works and make the app in the way we want

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import "@/global.css";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-4 text-lg text-primary bg-surface-elevated p-2 rounded-lg"
+      >
+        Sign In
+      </Link>
+      <Link
+        href="/(auth)/sign-up"
+        className="mt-4 text-lg text-primary bg-surface-elevated p-2 rounded-lg"
+      >
+        Sign Up
+      </Link>
+    </View>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,5 @@
-import "@/global.css";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
-export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link
-        href="/inventory"
-        className="mt-4 text-lg text-primary bg-surface-elevated p-2 rounded-lg"
-      >
-        Go to Inventory
-      </Link>
-      <Link
-        href="/(auth)/sign-in"
-        className="mt-4 text-lg text-primary bg-surface-elevated p-2 rounded-lg"
-      >
-        Sign In
-      </Link>
-      <Link
-        href="/(auth)/sign-up"
-        className="mt-4 text-lg text-primary bg-surface-elevated p-2 rounded-lg"
-      >
-        Sign Up
-      </Link>
-    </View>
-  );
+export default function Index() {
+  return <Redirect href="/(tabs)" />;
 }
